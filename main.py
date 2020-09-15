2a92344e5ae4c5f4434e0be27ef3566d58461d70
rubnsbarbosa <rubensbarbosa90@gmail.com>
2020-09-15 11:22:00 -0300

    main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import glob
 from PIL import Image
 import matplotlib.pyplot as plt
 from img_to_gray import image2gray
-from img_dilatation import dilatation
+from img_closing import closing
+from img_dilatation import dilation
 from img_segmentation import segmentation_Kmeans
 from img_granulometry import granulometry_opening, granulometry_closing
 
@@ -15,22 +16,25 @@ for image in glob.glob("img/*.jpg"):
     try:
         file_name, ext = os.path.splitext(image)
         print(file_name)
-        
         # open image
         img = Image.open(image).convert('RGB')
         img_gray = image2gray(img)
         # segmentation
         img_seg = segmentation_Kmeans(img_gray, num_centroids)
-        # dilatation in image gray
-        img_dilate = dilatation(img_gray, 5)
-        # now, applying segmentation in image dilated
-        img_seg_dilate = segmentation_Kmeans(img_dilate, num_centroids)
-        
-        # granulometry 
-        #img_open = granulometry_opening(img_seg, size=3)
-        #plt.contour(opened, colors='b', linewidths=2)
-        #plt.show()
 
+
+        # dilatation in image gray
+        img_dilate = dilation(img_gray, 5)
+        # applying segmentation in image dilated
+        img_seg_dilate = segmentation_Kmeans(img_dilate, num_centroids)
+
+
+        # closing in image gray
+        img_close = closing(img_gray, 5)
+        # applying segmentation in image closed
+        img_seg_close = segmentation_Kmeans(img_dilate, num_centroids)
+        
+ 
         # plots
         plt.figure(figsize=(12, 3.5))
         plt.title(file_name)
@@ -45,13 +49,13 @@ for image in glob.glob("img/*.jpg"):
         plt.axis('off')
 
         plt.subplot(143)
-        plt.imshow(img_seg, cmap=plt.cm.gray)
-        plt.title('Segmentation')
+        plt.imshow(img_close, cmap=plt.cm.gray)
+        plt.title('Closing')
         plt.axis('off')
 
         plt.subplot(144)
-        plt.imshow(img_seg_dilate, cmap=plt.cm.gray)
-        plt.title('Segmentation & Dilatation')
+        plt.imshow(img_seg_close, cmap=plt.cm.gray)
+        plt.title('Closing & Segmentation')
         plt.axis('off')
 
         plt.subplots_adjust(wspace=.05, left=.01, bottom=.01, right=.99, top=.99)
